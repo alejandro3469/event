b8bbfbf918a990c628a6bcadbd174b0e006df203
alejandro3469 <alejandro.perez3469@gmail.com>
2024-05-05 11:45:52 -0600

    info
    banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,31 @@
 import Image from "next/image";
+import { Allura, Tangerine, Lato, Merriweather } from "next/font/google";
 
+// If loading a variable font, you don't need to specify the font weight
+const dancing_script = Tangerine({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+const merriweather = Merriweather({
+  weight: ["300"],
+  subsets: ["latin"],
+});
 export default function Home() {
   return (
     <main>
-      <section>
-        <div>Claire & Sam</div>
-        <div>We joyfully invite you to the celebration of our marriage</div>
-        <div>
-          <span className="day">Saturday</span>
-          <div>
-            <span className="month">JUNE</span>
-            <span className="daynumber">08</span>
-            <span className="year">2024</span>
+      <section className="banner">
+        <div className={`${merriweather.className} info`}>
+          <div className="message">The wedding of</div>
+          <div className="image_container">
+            <div className={`${dancing_script.className} names`}>
+              <span className="first_person">Lilian & </span>
+              <span className="second_person">Alejandro</span>
+            </div>
+            <img src="/wed/Lake Michigan Beach Engagement Photos at Sunset.jpg" />
+            <div className="date">December 10, 2026 </div>
           </div>
-          <span className="hour">2:30pm</span>
-        </div>
-        <div className="address">
-          <span className="addresslineone">The polo lounge</span>
-          <span className="addresslinetwo">2910 New ridge RD Elliston VA 24087</span>
+          <div className="join_us">Join us</div>
+          <div className="address">Melchor Ocampo, MX | 5:00pm</div>
         </div>
       </section>
     </main>
