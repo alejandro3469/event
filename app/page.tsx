13e965146cad0b129192a60729d11be0b356efed
alejandro3469 <alejandro.perez3469@gmail.com>
2024-05-05 22:24:07 -0600

    structure

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,10 @@ export default function Home() {
               <span className="first_person">Lilian & </span>
               <span className="second_person">Alejandro</span>
             </div>
-            <img className="image" src="/wed/Lake Michigan Beach Engagement Photos at Sunset.jpg" />
+            <img
+              className="image"
+              src="/wed/Lake Michigan Beach Engagement Photos at Sunset.jpg"
+            />
 
             <div className="date">
               <span className="date_line" />
@@ -33,22 +36,26 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="banner section2">
+      <section className="banner details">
         <div className={`${merriweather.className} info`}>
-          <div className="message">The wedding of</div>
+          <div className="message">The details</div>
           <div className="image_container">
-            <div className={`${dancing_script.className} names`}>
-              <span className="first_person">Lilian & </span>
-              <span className="second_person">Alejandro</span>
+            <div className="full_names">
+              <span className="full_name name1">
+                <span>Lilian </span>
+                <span>Aguilar</span>
+              </span>
+              <span className="full_name name2">
+                <span>Alejandro </span>
+                <span>Perez</span>
+              </span>
             </div>
-            <img className="image" src="/wed/Lake Michigan Beach Engagement Photos at Sunset.jpg" />
-            <div className="date">
-              <span className="date_line" />
-              <span>December 10, 2026</span>
-            </div>
+            <img
+              className="image"
+              src="/wed/Lake Michigan Engagement Photos by Tylee Shay Photography (9).jpg"
+            />
           </div>
-          <div className="join_us">Join us</div>
-          <div className="address">Melchor Ocampo, MX | 5:00pm</div>
+          <div className="join_us">{"Can't wait to celebrate with you"}</div>
         </div>
       </section>
     </main>
